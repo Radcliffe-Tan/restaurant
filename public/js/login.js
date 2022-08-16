0d98c8bd857e4310312454515bb9e1e5985dc549
Radcliffe-Tan <83255904+Radcliffe-Tan@users.noreply.github.com>
2022-08-16 22:37:53 +0800

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ function loginMe(){
 
     var loginUser = new XMLHttpRequest();
 
-    loginUser.open("POST", "http://ec2-3-85-51-19.compute-1.amazonaws.com:8080/login", true)
+    loginUser.open("POST", "http://ec2-54-234-197-49.compute-1.amazonaws.com:8080/login", true)
     loginUser.setRequestHeader("Content-Type", "application/json")
     loginUser.onload = function () 
         {
