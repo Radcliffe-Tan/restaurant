3af39ee03cd92bf959d120461c02198974a439ce
Radcliffe-Tan <83255904+Radcliffe-Tan@users.noreply.github.com>
2022-08-18 16:18:23 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -15,5 +15,5 @@ var thumbsup = 'images/thumbsup.png';
 var thumbsupcolor = 'images/thumbsupcolor.png';
 var rating = 0;
 
-var translate_url = new URL("https://py2ab7e8rj.execute-api.us-east-1.amazonaws.com/default");
+var translate_url = new URL("https://48302r5119.execute-api.us-east-1.amazonaws.com/translation/");
 var newText = "";
