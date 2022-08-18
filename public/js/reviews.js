7e80068fdb162bd450ecad7f6297753ff55642ed
Radcliffe-Tan <83255904+Radcliffe-Tan@users.noreply.github.com>
2022-08-18 23:08:40 +0800

    Update reviews.js

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -59,6 +59,8 @@ function newReview() {
 
 function addReview() {
 
+    console.log(addReview());
+    
     var token = sessionStorage.getItem("token"); //Check for session storage
     console.log(token);
     if (token != null) {
@@ -214,4 +216,25 @@ function deleteReview(element)
         else {
             $('#reviewfailModal').modal('show')
             }
+}
+
+function notification(){
+
+    var notification = new XMLHttpRequest();
+    console.log("please work")
+    notification.open("POST", "https://hjgkflrqu8.execute-api.us-east-1.amazonaws.com/default/SNS",true);
+    notification.setRequestHeader("Content-Type", "application/json");
+
+    userid = sessionStorage.getItem("userid");
+    username = sessionStorage.getItem("username");
+    reviewmessage = document.getElementById("reviewBody").value;
+
+    notification.onload = function () {
+        log = JSON.parse(notification.responseText);
+        console.log(log);
+    };
+    
+    var payload = {userid:userid,username:username,reviewmessage:reviewmessage};
+    console.log(payload)
+    notification.send(JSON.stringify(payload));
 }
