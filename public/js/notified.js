e69fbb906cc912f33ecd80fb2012b786f064a306
Radcliffe-Tan <83255904+Radcliffe-Tan@users.noreply.github.com>
2022-08-18 22:47:16 +0800

    Update notified.js

--- public/js/notified.js
+++ public/js/notified.js
@@ -1,16 +1,16 @@
-function notification(){
+// function notification(){
 
-    var notification = new XMLHttpRequest();
-    console.log("notified")
-    notification.open("POST", "https://hjgkflrqu8.execute-api.us-east-1.amazonaws.com/default/SNS",true);
-    notification.setRequestHeader("Content-Type", "application/json");
+//     var notification = new XMLHttpRequest();
+//     console.log("notified")
+//     notification.open("POST", "https://hjgkflrqu8.execute-api.us-east-1.amazonaws.com/default/SNS",true);
+//     notification.setRequestHeader("Content-Type", "application/json");
 
-    userid = sessionStorage.getItem("userid");
-    username = sessionStorage.getItem("username");
-    reviewmessage = document.getElementById("reviewBody").value;
+//     userid = sessionStorage.getItem("userid");
+//     username = sessionStorage.getItem("username");
+//     reviewmessage = document.getElementById("reviewBody").value;
 
-    var payload = {userid:userid,username:username,reviewmessage:reviewmessage};
-    console.log(payload)
-    notification.send(JSON.stringify(payload));
+//     var payload = {userid:userid,username:username,reviewmessage:reviewmessage};
+//     console.log(payload)
+//     notification.send(JSON.stringify(payload));
 
-}
+// }
